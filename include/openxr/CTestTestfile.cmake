f2fae7139478cb13635fea48ca934cbeead646e5
Vishal Nagpal <74981057+vishal980-glitch@users.noreply.github.com>
2025-09-29 18:32:58 +0530

    old work initializing
    old work initializing

---
+++ include/openxr/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: D:/New_Learn/Meta_Quest_software/Work/Day_01_Date_28-07-2024_OpenXR/SDK/OpenXR-SDK main/include/openxr
+# Build directory: D:/New_Learn/Meta_Quest_software/Work/Day_01_Date_28-07-2024_OpenXR/SDK/OpenXR-SDK main/include/openxr
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
---
+++ src/CTestTestfile.cmake
@@ -0,0 +1,7 @@
+# CMake generated Testfile for 
+# Source directory: D:/New_Learn/Meta_Quest_software/Work/Day_01_Date_28-07-2024_OpenXR/SDK/OpenXR-SDK main/src
+# Build directory: D:/New_Learn/Meta_Quest_software/Work/Day_01_Date_28-07-2024_OpenXR/SDK/OpenXR-SDK main/src
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+subdirs("loader")
